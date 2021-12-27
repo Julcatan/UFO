7faa933c110d09528eb7419748f668086a6fe900
Cathleen Mai <91682586+Julcatan@users.noreply.github.com>
2021-12-27 15:25:34 -0500

    commit 12272021 first draft challenge

--- static/js/app.js
+++ static/js/app.js
@@ -23,44 +23,112 @@ function buildTable(data) {
   });
 }
 
+
 // 1. Create a variable to keep track of all the filters as an object.
-
-
+var filters = {};
 // 3. Use this function to update the filters. 
 function updateFilters() {
-
+  
     // 4a. Save the element that was changed as a variable.
+    let changedDate = d3.select("datetime");
+    let changedCity = d3.select("city");
+    let changedState = d3.select("state");
+    let changedCountry = d3.select("country");
+    let changedShape = d3.select("shape");
+    //let changedElement = d3.select(this);
 
     // 4b. Save the value that was changed as a variable.
+    let dateValue =changedDate.property("value");
+    console.log(dateValue)
+    let cityValue=changedCity.property("value");
+    console.log(cityValue)
+    let stateValue=changedState.property("value");
+    console.log(stateValue)
+    let countryValue =changedCountry.property("value");
+    console.log(countryValue)
+    let shapeValue =changedShape.property("value");
+    console.log(shapeValue)
 
     // 4c. Save the id of the filter that was changed as a variable.
+    let filterIdDate= changedDate.attr("id");
+    console.log(filterIdDate);
+    let filterIdCity= changedCity.attr("id");
+    console.log(filterIdCity)
+    let filterIdState= changedState.attr("id");
+    console.log(filterIdState)
+    let filterIdCountry= changedCountry.attr("id");
+    console.log(filterIdCountry)
+    let filterIdShape= changedShape.attr("id");
+    console.log(filterIdShape)
 
-  
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
  
+  if (dateValue) {filters[filterIdDate]= dateValue}
+  else{
+    delete filters[filterIdDate];}
+    
+    if (cityValue) {filters[filterIdCity]= cityValue}
+    else {
+      delete filters[filterIdCity];}
+    
+      if (stateValue) {filters[filterIdState]= stateValue}
+      else {
+        delete filters[filterIdState];}
+ 
+        if (countryValue) {filters[filterIdCountry]= countryValue}
+        else {
+          delete filters[filterIdCountry];}
+ 
+          if (shapeValue) {filters[filterIdShape]= shapeValue}
+          else {
+            delete filters[filterIdShape];
+          };
+        
   
+
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
-  
-  }
+   
+      }
   
   // 7. Use this function to filter the table when data is entered.
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    
+    let filteredData = tableData; 
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
-  
+  data.forEach(function (data)
+     {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);})
+
+      {
+        filteredData = filteredData.filter(row => row.datetime === city);}
+
+         {
+          filteredData = filteredData.filter(row => row.datetime === state);}
+
+
+           {
+            filteredData = filteredData.filter(row => row.datetime === country);}
+
+             {
+              filteredData = filteredData.filter(row => row.datetime === shape);}
+
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
-  
-  
+  d3.selectAll("datetime").on("change", updateFilters);
+  d3.selectAll("city").on("change", updateFilters);
+  d3.selectAll("state").on("change", updateFilters);
+  d3.selectAll("country").on("change", updateFilters);
+  d3.selectAll("shape").on("change", updateFilters);
+  //d3.selectAll("input").on("change", updateFilters);
   // Build the table when the page loads
   buildTable(tableData);
